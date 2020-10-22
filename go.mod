66d2807aa1c13871f46e5f01a2343ba753f3dc57
zhuxianglong <56494565@qq.com>
2020-10-22 20:38:53 +0800

    websocket

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-redis/redis/v8 v8.3.1
 	github.com/google/uuid v1.1.2
+	github.com/gorilla/websocket v1.4.2
 	github.com/grokify/html-strip-tags-go v0.0.0-20200923094847-079d207a09f1
 	github.com/jordan-wright/email v0.0.0-20200917010138-e1c00e156980
 	github.com/natefinch/lumberjack v2.0.0+incompatible
---
+++ net/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+var upGrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func main() {
+	bindAddress := "127.0.0.1:2303"
+	r := gin.Default()
+	r.GET("/ping", ping)
+	r.Run(bindAddress)
+}
+
+//webSocket请求ping 返回pong
+func ping(c *gin.Context) {
+	//升级get请求为webSocket协议
+	ws, err := upGrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		return
+	}
+	defer ws.Close()
+	for {
+		//读取ws中的数据
+		mt, message, err := ws.ReadMessage()
+		if err != nil {
+			break
+		}
+		if string(message) == "ping" {
+			message = []byte("pong")
+		}
+		//写入ws数据
+		err = ws.WriteMessage(mt, message)
+		if err != nil {
+			break
+		}
+	}
+}
