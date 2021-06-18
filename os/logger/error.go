d2b9defba39e3c5aeabaf5c2e834ce8185700f5b
zhuxianglong <56494565@qq.com>
2021-06-18 18:13:41 +0800

    添加Gin日志中间件

---
+++ os/logger/error.go
@@ -0,0 +1,65 @@
+package logger
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"runtime/debug"
+	"strings"
+)
+
+// Recovery Catch the exception and write it to the log(捕获异常并且写入日志)
+func Recovery() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				request := c.Request
+				var body []byte
+				requestBody := make(map[string]interface{})
+
+				if request.Body != nil {
+					body, _ = ioutil.ReadAll(request.Body)
+					// 把刚刚读出来的再写进去其他地方使用没有
+					c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+					//解析Body
+					if request.Header["Content-Type"] != nil && request.Header["Content-Type"][0] == "application/json" {
+						json.Unmarshal(body, &requestBody)
+					} else if len(body) > 0 {
+						bodyList := strings.Split(string(body), "&")
+						for i := 0; i < len(bodyList); i++ {
+							value := strings.Split(bodyList[i], "=")
+							requestBody[value[0]] = value[1]
+						}
+					}
+				}
+				// 请求URL
+				path, _ := url.QueryUnescape(c.Request.URL.RequestURI())
+				// 请求类型
+				method := request.Method
+				// 请求IP
+				ip := c.ClientIP()
+
+				Write.Error("错误报错",
+					zap.Any("ip", ip),
+					zap.Any("path", path),
+					zap.Any("request", requestBody),
+					zap.Any("method", method),
+					zap.Any("header", request.Header),
+					zap.Any("err", err),
+					zap.Any("stack", debug.Stack()),
+				)
+				c.AbortWithStatus(http.StatusInternalServerError)
+				log.Println(err)
+				debug.PrintStack()
+			}
+		}()
+
+		// 继续往下处理
+		c.Next()
+	}
+}
---
+++ os/logger/request.go
@@ -0,0 +1,68 @@
+package logger
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"io/ioutil"
+	"net/url"
+	"strings"
+	"time"
+)
+
+//Request Middleware logging module(中间件记录日志模块)
+func Request() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		request := c.Request
+		//请求状态不是OPTIONS和日志开启
+		if request.Method != "OPTIONS" {
+			var body []byte
+			requestBody := make(map[string]interface{})
+
+			if c.Request.Body != nil {
+				body, _ = ioutil.ReadAll(c.Request.Body)
+				// 把刚刚读出来的再写进去其他地方使用没有
+				c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+				//解析body
+				if request.Header["Content-Type"] != nil && request.Header["Content-Type"][0] == "application/json" {
+					json.Unmarshal(body, &requestBody)
+				} else if len(body) > 0 {
+					bodyList := strings.Split(string(body), "&")
+					for i := 0; i < len(bodyList); i++ {
+						value := strings.Split(bodyList[i], "=")
+						requestBody[value[0]] = value[1]
+					}
+				}
+			}
+
+			// 开始时间
+			start := time.Now()
+			// 处理请求
+			c.Next()
+			// 结束时间
+			end := time.Now()
+			// 执行时间
+			run_time := end.Sub(start)
+			// 请求URL
+			path, _ := url.QueryUnescape(c.Request.URL.RequestURI())
+			// 请求IP
+			ip := c.ClientIP()
+			// 请求类型
+			method := c.Request.Method
+			// 请求状态
+			status := c.Writer.Status()
+
+			// 写入日志
+			Write.Info("RESTful Request",
+				zap.Any("ip", ip),
+				zap.Any("path", path),
+				zap.Any("request", requestBody),
+				zap.Any("run_time", run_time),
+				zap.Any("status", status),
+				zap.Any("method", method),
+				zap.Any("header", request.Header),
+			)
+		}
+	}
+}
