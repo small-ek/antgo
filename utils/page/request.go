33a4e7c6e169b0fe24b1e5d6db28cdad511582aa
zhuxianglong <56494565@qq.com>
2022-01-12 02:38:40 +0800

    request优化

---
+++ utils/page/request.go
@@ -0,0 +1,22 @@
+package page
+
+//PageParam Paging parameters
+type PageParam struct {
+	CurrentPage int                    `form:"current_page" json:"current_page" bson:"current_page" xml:"current_page" yaml:"current_page"`
+	PageSize    int                    `form:"page_size" json:"page_size" bson:"page_size" xml:"page_size" yaml:"page_size"`
+	Total       int64                  `form:"total" json:"total" bson:"total" xml:"total" yaml:"total"`
+	Filter      []string               `form:"filter[]" json:"filter[]" bson:"filter[]" xml:"filter[]" yaml:"filter[]"`
+	Order       string                 `form:"order" json:"order" bson:"order" xml:"order" yaml:"order"`
+	Select      []string               `form:"select[]" json:"select[]" bson:"select[]" xml:"select[]" yaml:"select[]"`
+	Group       string                 `form:"group" json:"group" bson:"group" xml:"group" yaml:"group"`
+	Omit        string                 `form:"omit" json:"omit" bson:"omit" xml:"omit" yaml:"omit"`
+	Extra       map[string]interface{} `form:"extra" json:"extra" bson:"extra" xml:"extra" yaml:"extra"`
+}
+
+//New Default pagination
+func New() PageParam {
+	return PageParam{
+		CurrentPage: 1,
+		PageSize:    10,
+	}
+}
