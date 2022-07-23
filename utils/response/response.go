6775ff26597f8fb4b697e28c9bb5297528d341b9
xianglong <56494565@qq.com>
2022-07-23 23:45:34 +0800

    结果返回

--- utils/response/response.go
+++ utils/response/response.go
@@ -8,17 +8,17 @@ import (
 
 const (
 	//ERROR Default error code returned
-	ERROR = 403
+	ERROR = 404
 	//SUCCESS Default success code return
 	SUCCESS = 200
 )
 
 //Write Return parameter
 type Write struct {
-	Code  int         `json:"code"`  //Code
-	Msg   string      `json:"msg"`   //Msg Prompt message
-	Data  interface{} `json:"data"`  //Data
-	Error string      `json:"error"` //Error message
+	StatusCode int         `json:"statusCode"` //Error Code
+	Msg        string      `json:"msg"`        //Msg Prompt message
+	Result     interface{} `json:"result"`     //Data
+	Error      string      `json:"error"`      //Error message
 }
 
 //Page Pagination return
@@ -28,11 +28,11 @@ type Page struct {
 }
 
 //ErrorResponse Error output
-func ErrorResponse(err error) *Write {
+func ErrorResponse(err string) *Write {
 	return &Write{
-		Code:  ERROR,
-		Msg:   "Error",
-		Error: err.Error(),
+		StatusCode: ERROR,
+		Msg:        "Error",
+		Error:      err,
 	}
 }
 
@@ -40,18 +40,18 @@ func ErrorResponse(err error) *Write {
 func Success(msg string, data ...interface{}) *Write {
 	var lenData = len(data)
 	if lenData == 1 {
-		return &Write{Code: SUCCESS, Msg: msg, Data: data[0]}
+		return &Write{StatusCode: SUCCESS, Msg: msg, Result: data[0]}
 	} else if lenData > 1 {
-		return &Write{Code: SUCCESS, Msg: msg, Data: data}
+		return &Write{StatusCode: SUCCESS, Msg: msg, Result: data}
 	}
-	return &Write{Code: SUCCESS, Msg: msg}
+	return &Write{StatusCode: SUCCESS, Msg: msg}
 }
 
 //Fail Error return, the second parameter is passed back to the front end and printed
-func Fail(msg string, err ...error) *Write {
+func Fail(msg string, err ...string) *Write {
 	if len(err) > 0 && config.Decode().Get("system.debug").Bool() == true {
-		logger.Write.Error("Return error", zap.Any("error", err[0].Error()))
-		return &Write{Code: ERROR, Msg: msg, Error: err[0].Error(), Data: ""}
+		logger.Write.Error("Return error", zap.Any("error", err))
+		return &Write{StatusCode: ERROR, Msg: msg, Error: err[0], Result: ""}
 	}
-	return &Write{Code: ERROR, Msg: msg}
+	return &Write{StatusCode: ERROR, Msg: msg, Error: err[0]}
 }
