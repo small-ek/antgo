3d3ce10298db5214f0fe71430e2b2b8241e83ffc
zhuxianglong <56494565@qq.com>
2022-08-18 11:31:12 +0800

    no message

--- utils/gin_cors/cors.go
+++ utils/gin_cors/cors.go
@@ -12,7 +12,7 @@ func Cors(c *gin.Context) {
 	c.Header("Access-Control-Allow-Credentials", "true")                              //服务器是否接受浏览器发送的Cookie
 	c.Header("Connection", "keep-alive")                                              //可以使一次TCP连接为同意用户的多次请求服务,提高了响应速度。
 	c.Header("Access-Control-Max-Age", "3600")                                        //多少秒以后再次OPTIONS.默认60分钟
-	c.Header("Access-Control-Allow-Headers", "Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-Requested-With")
+	c.Header("Access-Control-Allow-Headers", "*")
 	c.Header("Strict-Transport-Security", " max-age=63072000; includeSubdomains; preload")
 	//放行所有OPTIONS方法
 	if c.Request.Method == "OPTIONS" {
