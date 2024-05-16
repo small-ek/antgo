c2f951745cc2788af58d56562ad51b1543f5c1af
xianglong <56494565@qq.com>
2024-05-16 17:57:05 +0800

    分页参数优化

--- utils/page/request.go
+++ utils/page/request.go
@@ -2,16 +2,15 @@ package page
 
 // PageParam Paging parameters
 type PageParam struct {
-	CurrentPage int                    `form:"current_page" json:"current_page" bson:"current_page" xml:"current_page" yaml:"current_page"`
-	PageSize    int                    `form:"page_size" json:"page_size" bson:"page_size" xml:"page_size" yaml:"page_size"`
-	Total       int64                  `form:"total" json:"total" bson:"total" xml:"total" yaml:"total"`
-	Filter      []string               `form:"filter[]" json:"filter[]" bson:"filter[]" xml:"filter[]" yaml:"filter[]"`
-	Order       string                 `form:"order" json:"order" bson:"order" xml:"order" yaml:"order"`
-	Sort        string                 `form:"sort" json:"sort" bson:"sort" xml:"sort" yaml:"sort"`
-	Select      []string               `form:"select[]" json:"select[]" bson:"select[]" xml:"select[]" yaml:"select[]"`
-	Group       string                 `form:"group" json:"group" bson:"group" xml:"group" yaml:"group"`
-	Omit        string                 `form:"omit" json:"omit" bson:"omit" xml:"omit" yaml:"omit"`
-	Extra       map[string]interface{} `form:"extra" json:"extra" bson:"extra" xml:"extra" yaml:"extra"`
+	CurrentPage int                    `form:"current_page" json:"current_page" bson:"current_page" xml:"current_page" yaml:"current_page"` //当前页
+	PageSize    int                    `form:"page_size" json:"page_size" bson:"page_size" xml:"page_size" yaml:"page_size"`                //每页显示数量
+	Filter      []string               `form:"filter[]" json:"filter[]" bson:"filter[]" xml:"filter[]" yaml:"filter[]"`                     //过滤条件
+	Order       string                 `form:"order" json:"order" bson:"order" xml:"order" yaml:"order"`                                    //排序字段
+	Sort        string                 `form:"sort" json:"sort" bson:"sort" xml:"sort" yaml:"sort"`                                         //排序方式
+	Select      []string               `form:"select[]" json:"select[]" bson:"select[]" xml:"select[]" yaml:"select[]"`                     //选择字段
+	Group       string                 `form:"group" json:"group" bson:"group" xml:"group" yaml:"group"`                                    //分组字段
+	Omit        string                 `form:"omit" json:"omit" bson:"omit" xml:"omit" yaml:"omit"`                                         //忽略字段
+	Extra       map[string]interface{} `form:"extra" json:"extra" bson:"extra" xml:"extra" yaml:"extra"`                                    //额外参数
 }
 
 // New Default pagination
