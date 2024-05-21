be9dd8bc6214130aa9d88f878977ef1cc499488a
xianglong <56494565@qq.com>
2024-05-21 18:53:56 +0800

    中间件日志优化

--- frame/middleware/logger.go
+++ frame/middleware/logger.go
@@ -17,6 +17,11 @@ type responseBodyWriter struct {
 	body *bytes.Buffer
 }
 
+func (r responseBodyWriter) Write(b []byte) (int, error) {
+	r.body.Write(b)
+	return r.ResponseWriter.Write(b)
+}
+
 var api = sync.Pool{
 	New: func() interface{} {
 		return bytes.NewBuffer(make([]byte, 4096))
@@ -28,29 +33,31 @@ func Logger() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		buffer := api.Get().(*bytes.Buffer)
 		buffer.Reset()
+		defer func() {
+			if buffer != nil {
+				api.Put(buffer)
+				buffer = nil
+			}
+			c.Request.Body.Close()
+		}()
 
-		// 获取请求数据
 		requestBody := buffer.Bytes()
 		if c.Request.Body != nil {
-			// c.Request.Body 是一个 buffer 对象，只能读取一次
 			requestBody, _ = io.ReadAll(c.Request.Body)
-			// 读取后，重新赋值 c.Request.Body ，以供后续的其他操作
 			c.Request.Body = io.NopCloser(bytes.NewBuffer(requestBody))
 		}
-		// 获取 response 内容
+
 		w := &responseBodyWriter{body: buffer, ResponseWriter: c.Writer}
 		c.Writer = w
-		// 设置开始时间
 		startTime := time.Now()
 		c.Next()
 
-		// 开始记录日志的逻辑
 		endTime := time.Now()
-		responStatus := c.Writer.Status()
+		responseStatus := c.Writer.Status()
 		// 请求URL
 		path, _ := url.QueryUnescape(c.Request.URL.RequestURI())
 		logFields := []zap.Field{
-			zap.Int("status", responStatus),
+			zap.Int("status", responseStatus),
 			zap.String("path", path),
 			zap.String("method", c.Request.Method),
 			zap.Any("header", c.Request.Header),
@@ -58,20 +65,17 @@ func Logger() gin.HandlerFunc {
 			zap.String("errors", c.Errors.ByType(gin.ErrorTypePrivate).String()),
 			zap.String("time", endTime.Sub(startTime).String()),
 		}
-		isNotOptions := c.Request.Method != "OPTIONS"
-		if isNotOptions {
+		if c.Request.Method != "OPTIONS" {
 			logFields = append(logFields, zap.Any("request-body", requestBody))
 			logFields = append(logFields, zap.Any("response-body", w.body.String()))
 		}
 
-		logger := alog.Write
-		if responStatus > 400 && responStatus <= 499 {
-			// 除了 StatusBadRequest 以外，warning 提示一下，常见的有 403 404，开发时都要注意
-			logger.Warn("HTTP Warning "+cast.ToString(responStatus), logFields...)
+		if responseStatus > 400 && responseStatus <= 499 {
+			alog.Write.Warn("HTTP Warning "+cast.ToString(responseStatus), logFields...)
 		} else {
-			logger.Debug("HTTP Access Log", logFields...)
+			alog.Write.Debug("HTTP Access Log", logFields...)
 		}
 		api.Put(buffer)
-		c.Request.Body.Close()
+		buffer = nil
 	}
 }
