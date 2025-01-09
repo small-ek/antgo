426e5b15c7c8c12f2efe43d51e76a61778af4404
zhuxianglong <56494565@qq.com>
2025-01-09 23:34:52 +0800

    返回数据结构优化

--- utils/response/response.go
+++ utils/response/response.go
@@ -2,39 +2,39 @@ package response
 
 // Write Return parameter 返回参数
 type Write struct {
-	Code    int         `json:"code"`            //Status Code 业务逻辑的状态码
-	Message string      `json:"message"`         //Msg Prompt message 简短描述
-	Data    interface{} `json:"data,omitempty"`  //Data 实际数据
-	Error   interface{} `json:"error,omitempty"` //Error message 错误相关的信息
+	Status  int    `json:"status"`          // 0: 成功, 1: 失败
+	Code    string `json:"code"`            // 业务状态码
+	Message string `json:"message"`         //用户友好提示信息
+	Data    any    `json:"data"`            //Data 实际数据
+	Error   any    `json:"error,omitempty"` //Error message 错误相关的信息
 }
 
 // Page Pagination return 分页返回
 type Page struct {
-	Total int64       `json:"total"` //Total pages 总数量
-	Items interface{} `json:"items"` //Data set of the current page 数据集合
+	Total int64 `json:"total"` //Total pages 总数量
+	Items any   `json:"items"` //Data set of the current page 数据集合
 }
 
 // Success Successfully returned 成功返回
-func Success(msg string, code int, data ...interface{}) *Write {
-	var lenData = len(data)
-	if lenData == 1 {
-		return &Write{Code: code, Message: msg, Data: data[0]}
-	} else if lenData > 1 {
-		return &Write{Code: code, Message: msg, Data: data}
+func Success(code, msg string, data ...any) *Write {
+	var resultData interface{}
+	if len(data) == 1 {
+		resultData = data[0]
+	} else if len(data) > 1 {
+		resultData = data
 	}
 
-	return &Write{Code: code, Message: msg}
+	return &Write{Status: 0, Code: code, Message: msg, Data: resultData}
 }
 
 // Fail Error return 错误返回
-func Fail(msg string, code int, err ...string) *Write {
-	var lenErr = len(err)
-
-	if lenErr == 1 {
-		return &Write{Code: code, Message: msg, Error: err[0]}
-	} else if lenErr > 1 {
-		return &Write{Code: code, Message: msg, Error: err}
+func Fail(code, msg string, err ...string) *Write {
+	var errorData any
+	if len(err) == 1 {
+		errorData = err[0]
+	} else if len(err) > 1 {
+		errorData = err
 	}
 
-	return &Write{Code: code, Message: msg}
+	return &Write{Status: 1, Code: code, Message: msg, Error: errorData}
 }
